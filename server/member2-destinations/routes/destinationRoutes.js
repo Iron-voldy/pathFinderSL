7ff4c4397efb203e380d29c3ba05bc8a47dbb06a
Yasas <yasaswijeratne200426@gmail.com>
2026-04-26 22:48:03 +0530

    destination image updates

--- server/member2-destinations/routes/destinationRoutes.js
+++ server/member2-destinations/routes/destinationRoutes.js
@@ -3,11 +3,15 @@ const upload = require('../../config/upload');
 const {
   createDestinationHandler,
   listDestinationsHandler,
+  updateDestinationHandler,
+  deleteDestinationHandler,
 } = require('../controllers/destinationController');
 
 const router = express.Router();
 
 router.get('/', listDestinationsHandler);
 router.post('/', upload.single('image'), createDestinationHandler);
+router.put('/:id', updateDestinationHandler);
+router.delete('/:id', deleteDestinationHandler);
 
 module.exports = router;
